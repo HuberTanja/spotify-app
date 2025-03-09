eaaab8968d0b8c9fc20245e813cd60c6a8cc05e4
Simeon <s.hoertenhuber@htblaleonding.onmicrosoft.com>
2025-03-09 13:09:07 +0100

    button click changes to the next song

--- test/script.js
+++ test/script.js
@@ -1,84 +1,88 @@
-// DOM
 const swiper = document.querySelector('#swiper');
-const like = document.querySelector('#like');
-const dislike = document.querySelector('#dislike');
+        const like = document.querySelector('#like');
+        const dislike = document.querySelector('#dislike');
 
-// constants
-const albumData = [
-  {
-    artist: "Kendrick Lamar",
-    coverUrl: "./img/gnx.jpeg",
-    song: "peekaboo - (Kendrick Lamar)"
-  },
-  {
-    artist: "Joji",
-    coverUrl: "./img/joji.jpeg",
-    song: "Slow Dancing in the Dark - (Joji)"
-  },
-  {
-    artist: "XXXTentacion",
-    coverUrl: "./img/xxx.jpeg",
-    song: "Teeth - (XXXTentacion)"
-  },
-  {
-    artist: "Still Woozy",
-    coverUrl: "./img/still_woozy.jpeg",
-    song: "Goodie Bag - (Still Woozy)"
-  },
-  {
-    artist: "Mac Miller",
-    coverUrl: "./img/mac_miller.jpeg",
-    song: "Self Care - (Mac Miller)"
-  }
-];
+        // constants
+        const albumData = [
+            {
+                artist: "Kendrick Lamar",
+                coverUrl: "./img/gnx.jpeg",
+                song: "peekaboo - (Kendrick Lamar)"
+            },
+            {
+                artist: "Joji",
+                coverUrl: "./img/joji.jpeg",
+                song: "Slow Dancing in the Dark - (Joji)"
+            },
+            {
+                artist: "XXXTentacion",
+                coverUrl: "./img/xxx.jpeg",
+                song: "Teeth - (XXXTentacion)"
+            },
+            {
+                artist: "Still Woozy",
+                coverUrl: "./img/still_woozy.jpeg",
+                song: "Goodie Bag - (Still Woozy)"
+            },
+            {
+                artist: "Mac Miller",
+                coverUrl: "./img/mac_miller.jpeg",
+                song: "Self Care - (Mac Miller)"
+            }
+        ];
 
-// variables
-let currentIndex = 0;
+        // variables
+        let currentIndex = 0;
 
-// functions
-function appendNewCard() {
-  if (currentIndex >= albumData.length) {
-    currentIndex = 0;
-  }
+        // functions
+        function appendNewCard() {
+            if (currentIndex >= albumData.length) {
+                currentIndex = 0;
+            }
 
-  const currentAlbum = albumData[currentIndex];
-  const card = new Card({
-    artist: currentAlbum.artist,
-    song: currentAlbum.song,  // Übergeben des Songtitels
-    imageUrl: currentAlbum.coverUrl,
-    onDismiss: loadNextCard,
-    onLike: () => {
-      like.style.animationPlayState = 'running';
-      like.classList.toggle('trigger');
-    },
-    onDislike: () => {
-      dislike.style.animationPlayState = 'running';
-      dislike.classList.toggle('trigger');
-    }
-  });
-  swiper.append(card.element);
-  currentIndex++;
-}
+            const currentAlbum = albumData[currentIndex];
+            const card = new Card({
+                artist: currentAlbum.artist,
+                song: currentAlbum.song,
+                imageUrl: currentAlbum.coverUrl,
+                onDismiss: loadNextCard,
+                onLike: () => {
+                    like.style.animationPlayState = 'running';
+                    like.classList.toggle('trigger');
+                },
+                onDislike: () => {
+                    dislike.style.animationPlayState = 'running';
+                    dislike.classList.toggle('trigger');
+                }
+            });
+            card.element.cardInstance = card;
+            swiper.append(card.element);
+            currentIndex++;
+        }
 
-function loadNextCard() {
-  appendNewCard();
-}
+        function loadNextCard() {
+            appendNewCard();
+        }
 
-// Load only the first card
-appendNewCard();
+        function swipeRight() {
+            const card = swiper.querySelector('.card:last-child');
+            if (card) {
+                const cardInstance = card.cardInstance;
+                if (cardInstance) {
+                    cardInstance.swipe(1);
+                }
+            }
+        }
 
-function swipeRight(cardElement) {
-  cardElement.style.transition = 'transform 1s';
-  cardElement.style.transform = `translate(${window.innerWidth}px, 0) rotate(90deg)`;
-  setTimeout(() => {
-    cardElement.remove(); // Entfernt die Karte nach der Animation
-  }, 1000);
-}
+        function swipeLeft() {
+            const card = swiper.querySelector('.card:last-child');
+            if (card) {
+                const cardInstance = card.cardInstance;
+                if (cardInstance) {
+                    cardInstance.swipe(-1);
+                }
+            }
+        }
 
-function swipeLeft(cardElement) {
-  cardElement.style.transition = 'transform 1s';
-  cardElement.style.transform = `translate(-${window.innerWidth}px, 0) rotate(-90deg)`;
-  setTimeout(() => {
-    cardElement.remove(); // Entfernt die Karte nach der Animation
-  }, 1000);
-}
+        // Load only the first card
+        appendNewCard();

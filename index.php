49dcfc70c4e69913c0c3a58ebb26d8fd13b1ce27
Binder Dominik <d.binder@htblaleonding.onmicrosoft.com>
2025-03-13 08:31:36 +0100

    simple styling

--- index.php
+++ index.php
@@ -122,6 +122,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'playlist') {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style/main.css">
         <title><?= htmlspecialchars($playlist['name']) ?></title>
     </head>
     <body>
